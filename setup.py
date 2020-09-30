d5232be4fd72308de0fdfb0bb55e9ae7832e3c08
Hair Albeiro Parra Barrera <jair.parra@outlook.com>
2020-09-29 21:59:33 -0400

    package init

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 21:13:03 2020
+
+@author: jairp
+"""
+
+###############################################################################
+### 1. Imports ### 
+import setuptools 
+
+##############################################################################
+### 2. Setting up ### 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="catplotlib-jairparra", # Replace with your own username
+    version="0.0.1",
+    author="Hair Parra",
+    author_email="jair.parra@outlook.com",
+    description="Matplotlib. But with cats. ฅ(＾・ω・＾ฅ)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject", ## TODO: Update with proj repository 
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
